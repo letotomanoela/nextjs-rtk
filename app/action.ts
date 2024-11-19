02db0778d59d68aa621adbdd7af564f9dfaabf29
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2024-11-19 10:32:25 +0300

    final

--- app/action.ts
+++ app/action.ts
@@ -5,3 +5,22 @@ export const getPostsActions = async () => {
   const posts = await prisma.post.findMany();
   return posts;
 };
+
+type PostBody = {
+  title: string;
+  content: string;
+  authorId: string;
+};
+export const createPost = async (body: PostBody) => {
+  const post = await prisma.post.create({
+    data: {
+      ...body,
+    },
+  });
+  return post;
+};
+
+export const getAuthor = async () => {
+  const author = await prisma.user.findMany();
+  return author;
+};
--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,22 +1,111 @@
 "use client";
 
 import { Post } from "@prisma/client";
-import { useQuery } from "@tanstack/react-query";
-import { getPostsActions } from "../action";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createPost, getAuthor, getPostsActions } from "../action";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 const PostPage = () => {
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
   const { data: posts, isLoading } = useQuery({
     queryKey: ["posts"],
     queryFn: getPostsActions,
   });
 
+  const { data: authors, isLoading: loadingAuthor } = useQuery({
+    queryKey: ["author"],
+    queryFn: getAuthor,
+  });
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: createPost,
+    mutationKey: ["create-post"],
+  });
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [authorId, setAuthorId] = useState("");
+
+  const onSubmit = async () => {
+    try {
+      await mutateAsync({
+        title,
+        content,
+        authorId: authorId,
+      });
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      toast({
+        title: "Post created",
+        variant: "default",
+      });
+      setTitle("");
+      setContent("");
+      setAuthorId("");
+    } catch (error) {
+      toast({
+        title: "Something went wrong",
+        variant: "destructive",
+      });
+    }
+  };
+
   if (isLoading) return <div>Loading...</div>;
+  if (loadingAuthor) return <div>Loading Author...</div>;
 
   return (
-    <div>
-      {posts?.map((item: Post) => (
-        <div key={item.id}>{item.title}</div>
-      ))}
+    <div className="p-12 w-full grid gap-x-16 grid-cols-2">
+      <div className="space-y-5">
+        <h1>Add post</h1>
+        <Input
+          disabled={isPending}
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="title"
+        />
+        <Input
+          disabled={isPending}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="content"
+        />
+        <Select onValueChange={(e) => setAuthorId(e)} defaultValue={authorId}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Author" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Select Author</SelectLabel>
+              {authors?.map((item) => (
+                <SelectItem key={item.id} value={item.id}>
+                  {item.name}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button onClick={onSubmit} disabled={isPending}>
+          {isPending ? "Loading..." : "Add Post"}
+        </Button>
+      </div>
+      <div>
+        <h1>List of posts</h1>
+        {posts?.map((item: Post) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
